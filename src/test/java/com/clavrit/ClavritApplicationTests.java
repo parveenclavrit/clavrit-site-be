191a94a1d05f40758f53fa5bc6e3b97503b0ad6c
parveengit12 <parveen@clavrit.com>
2025-06-24 16:53:23 +0530

    initial commit

---
+++ src/test/java/com/clavrit/ClavritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clavrit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClavritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
