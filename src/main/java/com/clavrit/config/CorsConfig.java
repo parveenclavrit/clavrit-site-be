507fca8cd291e8b927d25ceb083d9b685837c4f3
parveengit12 <parveen@clavrit.com>
2025-07-17 16:48:19 +0530

    cors issue resolved.

--- src/main/java/com/clavrit/config/CorsConfig.java
+++ src/main/java/com/clavrit/config/CorsConfig.java
@@ -1,42 +1,25 @@
 package com.clavrit.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-public class CorsConfig {
+	@Configuration
+	public class CorsConfig {
+		
 
- 
-//	@Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/*") 
-//                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                    .allowedHeaders("*")
-//                    .allowedOrigins("*");
-//            }
-//        };
-//    }
-	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+		@Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**") // Adjust the mapping as needed
+	                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+	                    .allowedHeaders("*")
+	                    .allowedOrigins("*"); // Adjust origins as needed
+	            }
+	        };
+	    }
 
- 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
- 
 }
 
